610ee54df21119f3a033005d6c8637ec92d3d738
programminglives <tmg.sun-j@hotmail.com>
2024-02-04 16:01:04 +0545

    featured products and category read on the home page

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 
 class FrontendController extends Controller
 {
 
     public function index()
     {
-        return view('frontend.index');
+        $categories = Category::withCount('products')->get();
+        $featuredProducts = Product::where('featured', true)->get();
+        return view('frontend.index', compact('categories', 'featuredProducts'));
     }
 
     public function contact()
